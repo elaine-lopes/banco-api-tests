8a1e310c8001344126272a51eae3a5e33c549e1d
elaine-lopes <elopess.rj@gmail.com>
2025-07-22 13:18:15 -0300

    Criei um helper de autenticação para obter o token

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -1,20 +1,14 @@
 const request = require('supertest');
 const { expect } = require('chai')
 require('dotenv').config()
+const { obterToken } = require ('../helpers/autenticacao')
+
 describe('Transferencias', () => {
     describe('POST /transferencias', () => {
         it('Deve retornar sucesso com 201 quando valor da transferência for igual ou acima de R$ 10,00', async () => {
-            const respostaLogin = await request(process.env.BASE_URL)
-                .post('/login')
-                .set('Content-Type', 'application/json')
-                .send({
-                    'username': 'julio.lima',
-                    'senha': '123456'
-                })  
+            const token = await obterToken('julio.lima', '123456')    
 
-            const token = respostaLogin.body.token    
-
-            const resposta = await request('http://localhost:3000')
+            const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
                 .set('Authorization', `Bearer ${token}`)
@@ -26,10 +20,25 @@ describe('Transferencias', () => {
                     token: ""
                    })
             
-                   expect(resposta.status).to.equal(201);  
+            expect(resposta.status).to.equal(201);  
             
         })  
         it('Deve retornar falha com 422 quando valor da tranferência for abaixo de R$ 10,00', async () => {
+            const token = await obterToken('julio.lima', '123456')   
+
+            const resposta = await request(process.env.BASE_URL)
+                .post('/transferencias')
+                .set('Content-Type', 'application/json')
+                .set('Authorization', `Bearer ${token}`)
+                //conta origem 1 e 2 são referentes aos IDs 1 e 2 da tabela contas do banco mysql
+                .send({
+                    contaOrigem: 1,
+                    contaDestino: 2,
+                    valor: 7,
+                    token: ""
+                })
+            
+            expect(resposta.status).to.equal(422); 
 
         })
     })
